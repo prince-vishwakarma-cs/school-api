93384cf308ab0eaa04b2097275d3aa19e85eddb3
prince-vishwakarma-cs <pv913177@gmail.com>
2025-08-20 22:53:38 +0530

    fix: Base url

--- api/index.js
+++ api/index.js
@@ -29,7 +29,14 @@ app.use('/api', schoolRoutes);
 app.get('/api', (req, res) => {
   res.status(200).json({
     success: true,
-    message: 'Welcome to the Serverless School Management API',
+    message: 'Welcome to the School Management API',
+  });
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to the School Management API',
   });
 });
 
